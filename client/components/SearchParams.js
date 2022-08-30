4c12795f86ea8f433057c0e443889923504bf436
mrkit <asmaffei@gmail.com>
2022-08-29 23:11:37 -0400

    added styling

--- client/components/SearchParams.js
+++ client/components/SearchParams.js
@@ -48,62 +48,60 @@ const SearchParams = () => {
     }
     
     return (
-        <main>
-            <section>
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="location">
-                        Location
-                        <input type="text" id="location" name="location" placeholder="Location" value={location} onChange={handleLocationChange} autoFocus/>
-                    </label>
+        <>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="location">
+                    Location
+                    <input type="text" id="location" name="location" placeholder="Location" value={location} onChange={handleLocationChange} autoFocus/>
+                </label>
 
-                    <label htmlFor="animal">
-                        Animal
-                        <select
-                            id="animal"
-                            value={animal}
-                            onChange={handleAnimalChange}
-                            onBlur={handleAnimalChange}
-                        >
-                            <option />
-                            {ANIMALS.map(animal => (
-                                <option key={animal} value={animal}>{animal}</option>
-                            ))}
-                        </select>
-                    </label>
+                <label htmlFor="animal">
+                    Animal
+                    <select
+                        id="animal"
+                        value={animal}
+                        onChange={handleAnimalChange}
+                        onBlur={handleAnimalChange}
+                    >
+                        <option />
+                        {ANIMALS.map(animal => (
+                            <option key={animal} value={animal}>{animal}</option>
+                        ))}
+                    </select>
+                </label>
 
-                    <label htmlFor="breed">
-                        Breed
-                        <select
-                            disabled={!breeds.length}
-                            id="breed"
-                            value={breed}
-                            onChange={handleBreedChange}
-                            onBlur={handleBreedChange}
-                        >
-                            <option />
-                            {breeds.map(breed => (
-                                <option key={breed} value={breed}>{breed}</option>
-                            ))}
-                        </select>
-                    </label>
+                <label htmlFor="breed">
+                    Breed
+                    <select
+                        disabled={!breeds.length}
+                        id="breed"
+                        value={breed}
+                        onChange={handleBreedChange}
+                        onBlur={handleBreedChange}
+                    >
+                        <option />
+                        {breeds.map(breed => (
+                            <option key={breed} value={breed}>{breed}</option>
+                        ))}
+                    </select>
+                </label>
 
-                    <label htmlFor="theme">
-                        Theme
-                        <select
-                            value={theme}
-                            onChange={handleThemeChange}
-                            onBlur={handleThemeChange}
-                        >
-                            <option value="darkblue">Dark Blue</option>
-                            <option value="thistle">Thistle</option>
-                        </select>
-                    </label>
+                <label htmlFor="theme">
+                    Theme
+                    <select
+                        value={theme}
+                        onChange={handleThemeChange}
+                        onBlur={handleThemeChange}
+                    >
+                        <option value="darkblue">Dark Blue</option>
+                        <option value="thistle">Thistle</option>
+                    </select>
+                </label>
 
-                    <button style={{ backgroundColor: theme }}>Submit</button>
-                </form>
-            </section>
+                <button style={{ backgroundColor: theme }}>Submit</button>
+            </form>
             <Results pets={pets} />
-        </main>
+        </>
     )
 };
 
