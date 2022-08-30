416b1639733454ae26b7caee487ed61757e084ce
mrkit <asmaffei@gmail.com>
2022-08-29 22:05:08 -0400

    fixed link to ThemeContext in index.js

--- client/index.js
+++ client/index.js
@@ -2,12 +2,12 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App';
-import ThemeContext from './ThemeContext';
+import ThemeContext from './components/ThemeContext';
 import './stylesheets/main.css';
 
 const Component = () => {
     const theme = useState("darkblue");
-    
+
     return (
         <StrictMode>
             <ThemeContext.Provider value={theme}>
