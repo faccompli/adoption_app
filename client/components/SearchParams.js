c718c58c119e0be00585d24533830efb2dd0e826
mrkit <asmaffei@gmail.com>
2022-08-29 22:39:26 -0400

    added input and selects to SearchParams

--- client/components/SearchParams.js
+++ client/components/SearchParams.js
@@ -1,5 +1,8 @@
 import { useState, useEffect } from 'react';
-import ThemseContext from './ThemeContext';
+import ThemeContext from './ThemeContext';
+import Results from './Results';
+
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
     const [location, setLocation] = useState("");
@@ -9,22 +12,96 @@ const SearchParams = () => {
     const [breeds] = useBreedList(animal);
     const [theme, setTheme] = useContext(ThemeContext);
 
+    useEffect(() => {
+        requestPets();
+    }, []);
+
+    async function requestPets(){
+        const res = await fetch(`http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`);
+        const json = await res.json();
+
+        setPets(json.pets);
+    }
+
     const handleSubmit = e => {
         e.preventDefault();
 
-        e.target.location.value = "";
+        requestPets();
     }
 
-    const handleChange = e => {
+    const handleLocationChange = e => {
         setLocation(e.target.value);
+    }
+
+    const handleAnimalChange = e => {
+        updateAnimal(e.target.value);
+        updateBreed("");
+    }
+
+    const handleBreedChange = e => {
+        updateBreed(e.target.value);
+    }
+
+    const handleThemeChange = e => {
+        setTheme(e.target.value);
     }
     
     return (
         <main>
-            <form onSubmit={handleSubmit}>
-                <input type="text" name="location" placeholder="Location" value={location} onChange={handleChange} autoFocus/>
-                <input type="submit" value="Submit"></input>
-            </form>
+            <section>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="location">
+                        Location
+                        <input type="text" id="location" name="location" placeholder="Location" value={location} onChange={handleLocationChange} autoFocus/>
+                    </label>
+
+                    <label htmlFor="animal">
+                        Animal
+                        <select
+                            id="animal"
+                            value={animal}
+                            onChange={handleAnimalChange}
+                            onBlur={handleAnimalChange}
+                        >
+                            <option />
+                            {ANIMALS.map(animal => (
+                                <option key={animal} value={animal}>{animal}</option>
+                            ))}
+                        </select>
+                    </label>
+
+                    <label htmlFor="breed">
+                        Breed
+                        <select
+                            disabled={!breeds.length}
+                            id="breed"
+                            value={breed}
+                            onChange={handleBreedChange}
+                            onBlur={handleBreedChange}
+                        >
+                            <option />
+                            {breeds.map(breed => (
+                                <option key={breed} value={breed}>{breed}</option>
+                            ))}
+                        </select>
+                    </label>
+
+                    <label htmlFor="theme">
+                        Theme
+                        <select
+                            value={theme}
+                            onChange={handleThemeChange}
+                            onBlur={handleThemeChange}
+                        >
+                            <option value="darkblue">Dark Blue</option>
+                            <option value="thistle">Thistle</option>
+                        </select>
+                    </label>
+
+                    <button style={{ backgroundColor: theme }}>Submit</button>
+                </form>
+            </section>
+            <Results pets={pets} />
         </main>
     )
 };
